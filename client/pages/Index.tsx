1214ab7e51b69f80cf65979f5a9c42b811891f7c
Jude <jude.sx@iinerds.com>
2025-08-22 18:37:33 +0530

    Remove debug code and restore clean production build

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -4,13 +4,8 @@ import ProjectGallery from "@/components/ProjectGallery";
 import Footer from "@/components/Footer";
 
 export default function Index() {
-  console.log('Index component loaded');
-  
   return (
     <div className="min-h-screen bg-background flex flex-col">
-      <div style={{ padding: '20px', background: 'red', color: 'white' }}>
-        DEBUG: App is loading! If you see this, the app is working.
-      </div>
       <Header />
       <main className="flex-1">
         <HeroSection />
